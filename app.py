45ad39fe775d45bcd807f9b2572637ba9741b51f
Aleksandr Borzunov <borzunov.alexander@gmail.com>
2023-01-08 02:12:10 +0000

    Use HTML template

--- app.py
+++ app.py
@@ -1,76 +1,20 @@
-import asyncio
-import threading
 from collections import defaultdict
 from functools import partial
 from dataclasses import dataclass, field
 from typing import List, Tuple
 
 import hivemind
-from async_timeout import timeout
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request
 from multiaddr import Multiaddr
-
 from petals.constants import PUBLIC_INITIAL_PEERS
 from petals.data_structures import ServerState
 from petals.dht_utils import get_remote_module_infos
 
-
-cache_lock = threading.Lock()
-reachable_cache = hivemind.TimedStorage()
-
-
-async def check_for_network_errors(
-    peer_id, _, node, *, connect_timeout = 5, expiration = 600, use_cache = True
-):
-    if use_cache and peer_id in reachable_cache:
-        with cache_lock:
-            return reachable_cache.get(peer_id).value
-
-    try:
-        with timeout(connect_timeout):
-            await node.p2p._client.connect(peer_id, [])
-            await node.p2p._client.disconnect(peer_id)
-
-        with cache_lock:
-            reachable_cache.store(peer_id, None, hivemind.get_dht_time() + expiration)
-        return None
-    except Exception as e:
-        if isinstance(e, asyncio.TimeoutError):
-            return f"Failed to connect in {connect_timeout:.0f} sec. Firewall may be blocking connections"
-        message = str(e)
-        message = message if message else repr(e)
-
-        with cache_lock:
-            reachable_cache.store(peer_id, message, hivemind.get_dht_time() + expiration)
-        return message
-
-
-async def get_network_errors(peer_ids, dht, node):
-    errors = await asyncio.gather(*[check_for_network_errors(peer_id, dht, node) for peer_id in peer_ids])
-    return {peer_id: err for peer_id, err in zip(peer_ids, errors) if err is not None}
+from p2p_utils import check_reachability, check_reachability_parallel
 
 
 dht = hivemind.DHT(initial_peers=PUBLIC_INITIAL_PEERS, client_mode=True, num_workers=32, start=True)
 app = Flask(__name__)
-
-@app.route("/")
-def main_page():
-    total_blocks = 70
-    module_infos = get_remote_module_infos(
-        dht, [f"bigscience/bloom-petals.{i}" for i in range(total_blocks)], float("inf"),
-    )
-    return f"<pre>{show_module_infos(module_infos, total_blocks)}</pre>"
-
-
-@app.route("/api/v1/is_reachable/<peer_id>")
-def api_v1_is_reachable(peer_id):
-    peer_id = hivemind.PeerID.from_base58(peer_id)
-    message = dht.run_coroutine(partial(check_for_network_errors, peer_id, use_cache=False))
-    return jsonify(
-        success=message is None,
-        message=message,
-        your_ip=request.remote_addr,
-    )
 
 
 @dataclass
@@ -80,7 +24,13 @@ class ServerInfo:
     blocks: List[Tuple[int, ServerState]] = field(default_factory=list)
 
 
-def show_module_infos(module_infos, total_blocks=70):
+@app.route("/")
+def main_page():
+    total_blocks = 70
+    module_infos = get_remote_module_infos(
+        dht, [f"bigscience/bloom-petals.{i}" for i in range(total_blocks)], float("inf"),
+    )
+
     servers = defaultdict(ServerInfo)
     n_found_blocks = 0
     for block_idx, info in enumerate(module_infos):
@@ -102,53 +52,64 @@ def show_module_infos(module_infos, total_blocks=70):
         if peer_id not in bootstrap_peer_ids:
             bootstrap_peer_ids.append(peer_id)
 
-    network_errors = dht.run_coroutine(partial(get_network_errors, bootstrap_peer_ids + list(servers.keys())))
+    network_errors = dht.run_coroutine(partial(check_reachability_parallel, bootstrap_peer_ids + list(servers.keys())))
     all_bootstrap_reachable = all(peer_id not in network_errors for peer_id in bootstrap_peer_ids)
 
     swarm_state = "healthy" if all_blocks_found and all_bootstrap_reachable else "broken"
-    lines = [
-        f"Swarm state: {swarm_state}\n",
-        f"Bootstrap peers: {''.join('_' if peer_id in network_errors else '#' for peer_id in bootstrap_peer_ids)}\n",
-        "Servers:\n",
-    ]
+    bootstrap_states = ''.join(
+        get_state_html("online" if peer_id not in network_errors else "unreachable")
+        for peer_id in bootstrap_peer_ids
+    )
 
     servers = sorted(servers.items(), key=lambda item: str(item[0]))
+    server_rows = []
     for peer_id, server_info in servers:
-        row_name = f'{peer_id}, {server_info.throughput:.1f} RPS'
-        row_name += ' ' * max(0, 63 - len(row_name))
-
-        if peer_id in network_errors:
-            state = "UNREACHABLE"
-
-        row = [' ' for _ in range(total_blocks)]
+        block_map = [' ' for _ in range(total_blocks)]
         for block_idx, state in server_info.blocks:
             if state == ServerState.OFFLINE:
-                row[block_idx] = '_'
                 if peer_id in network_errors:
                     del network_errors[peer_id]
-            elif peer_id in network_errors:
-                row[block_idx] = '?'
-            elif state == ServerState.JOINING:
-                row[block_idx] = 'J'
-            elif state == ServerState.ONLINE:
-                row[block_idx] = '#'
-        row = ''.join(row)
+            state_name = state.name if peer_id not in network_errors else "unreachable"
+            block_map[block_idx] = get_state_html(state_name)
+        block_map = ''.join(block_map)
 
-        lines.append(f"{row_name} |{row}|")
+        server_rows.append({
+            "peer_id": peer_id,
+            "throughput": server_info.throughput,
+            "block_map": block_map,
+        })
 
-    lines.extend([
-        "\nLegend:\n",
-        "# - online",
-        "J - joining     (loading blocks)",
-        "? - unreachable (port forwarding/NAT/firewall issues, see below)",
-        "_ - offline     (just disconnected)",
-    ])
+    reachability_issues = [
+        {"peer_id": peer_id, "err": err}
+        for peer_id, err in sorted(network_errors.items(), key=lambda item: str(item[0]))
+    ]
 
-    if network_errors:
-        lines.append("\nServer reachability issues:\n")
-        for peer_id, err in sorted(network_errors.items(), key=lambda item: str(item[0])):
-            lines.append(f'{peer_id} | {err}')
-        lines.append("\nPlease ask for help in #running-a-server if you are not sure how to fix this.")
+    return render_template(
+        "index.html",
+        swarm_state=swarm_state,
+        bootstrap_states=bootstrap_states,
+        server_rows=server_rows,
+        reachability_issues=reachability_issues,
+    )
 
-    return '\n'.join(lines)
 
+def get_state_html(state_name: str):
+    state_name = state_name.lower()
+    if state_name == "offline":
+        char = '_'
+    elif state_name == "unreachable":
+        char = '✖'
+    else:
+        char = '●'
+    return f'<span class="{state_name}">{char}</span>'
+
+
+@app.route("/api/v1/is_reachable/<peer_id>")
+def api_v1_is_reachable(peer_id):
+    peer_id = hivemind.PeerID.from_base58(peer_id)
+    message = dht.run_coroutine(partial(check_reachability, peer_id, use_cache=False))
+    return jsonify(
+        success=message is None,
+        message=message,
+        your_ip=request.remote_addr,
+    )
---
+++ p2p_utils.py
@@ -0,0 +1,38 @@
+import asyncio
+import threading
+
+import hivemind
+from async_timeout import timeout
+
+
+cache_lock = threading.Lock()
+reachable_cache = hivemind.TimedStorage()
+
+
+async def check_reachability(peer_id, _, node, *, connect_timeout = 5, expiration = 600, use_cache = True):
+    if use_cache and peer_id in reachable_cache:
+        with cache_lock:
+            return reachable_cache.get(peer_id).value
+
+    try:
+        with timeout(connect_timeout):
+            await node.p2p._client.connect(peer_id, [])
+            await node.p2p._client.disconnect(peer_id)
+
+        with cache_lock:
+            reachable_cache.store(peer_id, None, hivemind.get_dht_time() + expiration)
+        return None
+    except Exception as e:
+        if isinstance(e, asyncio.TimeoutError):
+            return f"Failed to connect in {connect_timeout:.0f} sec. Firewall may be blocking connections"
+        message = str(e)
+        message = message if message else repr(e)
+
+        with cache_lock:
+            reachable_cache.store(peer_id, message, hivemind.get_dht_time() + expiration)
+        return message
+
+
+async def check_reachability_parallel(peer_ids, dht, node):
+    errors = await asyncio.gather(*[check_reachability(peer_id, dht, node) for peer_id in peer_ids])
+    return {peer_id: err for peer_id, err in zip(peer_ids, errors) if err is not None}
