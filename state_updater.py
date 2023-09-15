e7f44e139c851a6f44b96821fa110e8b45b84b3a
Aleksandr Borzunov <borzunov.alexander@gmail.com>
2023-09-15 19:40:35 +0000

    Refactor with compute_spans()

--- state_updater.py
+++ state_updater.py
@@ -1,30 +1,23 @@
 import datetime
 import time
 import threading
-from collections import Counter, defaultdict
+from collections import Counter
 from contextlib import suppress
-from dataclasses import asdict, dataclass, field
+from dataclasses import asdict
 from functools import partial
-from typing import List, Optional, Tuple
 
 import hivemind
+import numpy as np
 from flask import Flask, render_template
 from multiaddr import Multiaddr
-from petals.data_structures import ServerInfo, ServerState
-from petals.utils.dht import get_remote_module_infos
+from petals.data_structures import UID_DELIMITER, ServerState
+from petals.utils.dht import compute_spans, get_remote_module_infos
 
 import config
 from data_structures import ModelInfo
 from p2p_utils import check_reachability_parallel
 
 logger = hivemind.get_logger(__name__)
-
-
-@dataclass
-class MergedServerInfo:
-    model: Optional[str] = None
-    blocks: List[Tuple[int, ServerState]] = field(default_factory=list)
-    server_info: Optional[ServerInfo] = None
 
 
 class StateUpdaterThread(threading.Thread):
@@ -59,13 +52,8 @@ class StateUpdaterThread(threading.Thread):
             if peer_id not in bootstrap_peer_ids:
                 bootstrap_peer_ids.append(peer_id)
 
-        rpc_infos = self.dht.run_coroutine(partial(check_reachability_parallel, bootstrap_peer_ids))
-        all_bootstrap_reachable = all(rpc_infos[peer_id]["ok"] for peer_id in bootstrap_peer_ids)
-
-        bootstrap_states = ["online" if rpc_infos[peer_id]["ok"] else "unreachable" for peer_id in bootstrap_peer_ids]
-        bootstrap_map = "".join(
-            f'<span class="{state_name}">{self._STATE_CHARS[state_name]}</span>' for state_name in bootstrap_states
-        )
+        reach_infos = self.dht.run_coroutine(partial(check_reachability_parallel, bootstrap_peer_ids))
+        bootstrap_states = ["online" if reach_infos[peer_id]["ok"] else "unreachable" for peer_id in bootstrap_peer_ids]
 
         models = config.MODELS[:]
         model_index = self.dht.get("_petals.models", latest=True)
@@ -85,102 +73,76 @@ class StateUpdaterThread(threading.Thread):
             models.extend(sorted(custom_models, key=lambda info: (-info.num_blocks, info.dht_prefix)))
         logger.info(f"Fetching info for models {[info.name for info in models]}")
 
-        block_ids = []
+        block_uids = [f"{model.dht_prefix}{UID_DELIMITER}{i}" for model in models for i in range(model.num_blocks)]
+        module_infos = get_remote_module_infos(self.dht, block_uids, latest=True)
+
+        model_servers = {}
+        all_servers = {}
+        offset = 0
         for model in models:
-            block_ids += [f"{model.dht_prefix}.{i}" for i in range(model.num_blocks)]
+            model_servers[model.dht_prefix] = compute_spans(
+                module_infos[offset:offset + model.num_blocks], min_state=ServerState.OFFLINE
+            )
+            all_servers.update(model_servers[model.dht_prefix])
+            offset += model.num_blocks
 
-        module_infos = get_remote_module_infos(
-            self.dht,
-            block_ids,
-            float("inf"),
-        )
-
-        servers = defaultdict(MergedServerInfo)
-        n_found_blocks = defaultdict(int)
-        for info in module_infos:
-            if info is None:
-                continue
-
-            dht_prefix, block_idx_str = info.uid.split('.')
-            found = False
-            for peer_id, server_info in info.servers.items():
-                servers[peer_id].model = dht_prefix
-                servers[peer_id].blocks.append((int(block_idx_str), server_info.state))
-                servers[peer_id].server_info = server_info
-                if server_info.state == ServerState.ONLINE:
-                    found = True
-            n_found_blocks[dht_prefix] += found
-
-        rpc_infos.update(
-            self.dht.run_coroutine(partial(check_reachability_parallel, list(servers.keys()), fetch_info=True))
+        online_servers = [peer_id for peer_id, span in all_servers.items() if span.state == ServerState.ONLINE]
+        reach_infos.update(
+            self.dht.run_coroutine(partial(check_reachability_parallel, online_servers, fetch_info=True))
         )
 
         top_contributors = Counter()
         model_reports = []
         for model in models:
-            all_blocks_found = n_found_blocks[model.dht_prefix] == model.num_blocks
-            model_state = "healthy" if all_blocks_found and all_bootstrap_reachable else "broken"
-
+            block_healthy = np.zeros(model.num_blocks, dtype=bool)
             server_rows = []
-            model_servers = [(peer_id, server) for peer_id, server in servers.items() if server.model == model.dht_prefix]
-            for peer_id, server in sorted(model_servers):
-                reachable = rpc_infos[peer_id]["ok"]
-                show_public_name = (
-                    len(server.blocks) >= 10 and
-                    all(state == ServerState.ONLINE for _, state in server.blocks) and reachable
-                )
-                if model.official and server.server_info.public_name and show_public_name:
-                    top_contributors[server.server_info.public_name] += len(server.blocks)
+            for peer_id, span in sorted(model_servers[model.dht_prefix].items()):
+                reachable = reach_infos[peer_id]["ok"] if peer_id in reach_infos else True
+                state = span.state.name.lower() if reachable else "unreachable"
+                if state == "online":
+                    block_healthy[span.start:span.end] = True
 
-                block_indices = [block_idx for block_idx, state in server.blocks if state != ServerState.OFFLINE]
-                block_indices = f"{min(block_indices)}:{max(block_indices) + 1}" if block_indices else ""
-
-                block_map = ['<td class="bm"> </td>' for _ in range(model.num_blocks)]
-                for block_idx, state in server.blocks:
-                    state_name = state.name.lower()
-                    if state == ServerState.ONLINE and not reachable:
-                        state_name = "unreachable"
-                    block_map[block_idx] = f'<td class="bm {state_name}">{self._STATE_CHARS[state_name]}</td>'
-                block_map = "".join(block_map)
+                show_public_name = state == "online" and span.length >= 10
+                if model.official and span.server_info.public_name and show_public_name:
+                    top_contributors[span.server_info.public_name] += span.length
 
                 row = {
                     "short_peer_id": "..." + str(peer_id)[-6:],
                     "peer_id": peer_id,
                     "show_public_name": show_public_name,
-                    "block_indices": block_indices,
-                    "block_map": block_map,
-                    "server_info": server.server_info,
-                    "adapters": [{"name": name, "short_name": name.split("/")[-1]} for name in server.server_info.adapters],
+                    "state": state,
+                    "span": span,
+                    "server_info": span.server_info,
+                    "adapters": [dict(name=name, short_name=name.split("/")[-1]) for name in span.server_info.adapters],
                     "pings_to_here": [
-                        {"source_id": source_id, "rtt": source.server_info.next_pings[str(peer_id)]}
-                        for source_id, source in model_servers
+                        dict(source_id=source_id, rtt=source.server_info.next_pings[str(peer_id)])
+                        for source_id, source in model_servers[model.dht_prefix].items()
                         if source.server_info.next_pings is not None and str(peer_id) in source.server_info.next_pings
                     ],
                 }
-                if server.server_info.cache_tokens_left is not None:
+                if span.server_info.cache_tokens_left is not None:
                     # We use num_blocks * 2 to account for both keys and values
-                    row["cache_tokens_left_per_block"] = server.server_info.cache_tokens_left // (len(server.blocks) * 2)
+                    row["cache_tokens_left_per_block"] = span.server_info.cache_tokens_left // (span.length * 2)
                 server_rows.append(row)
 
-            report = asdict(model)
-            report.update(name=model.name, short_name=model.short_name, state=model_state, server_rows=server_rows)
-            model_reports.append(report)
+            model_reports.append(dict(
+                name=model.name,
+                short_name=model.short_name,
+                state="healthy" if block_healthy.all() else "broken",
+                server_rows=server_rows,
+                **asdict(model)
+            ))
 
         reachability_issues = [
-            {"peer_id": peer_id, "err": info["error"]}
-            for peer_id, info in sorted(rpc_infos.items())
-            if not info["ok"]
-            and (peer_id not in servers or any(state == ServerState.ONLINE for _, state in servers[peer_id].blocks))
+            dict(peer_id=peer_id, err=info["error"]) for peer_id, info in sorted(reach_infos.items()) if not info["ok"]
         ]
 
         with self.app.app_context():
             self.last_state = render_template("index.html",
-                bootstrap_map=bootstrap_map,
+                bootstrap_states=bootstrap_states,
                 top_contributors=top_contributors,
                 model_reports=model_reports,
                 reachability_issues=reachability_issues,
                 last_updated=datetime.datetime.now(datetime.timezone.utc),
                 update_period=self.update_period,
             )
-
-    _STATE_CHARS = {"offline": "_", "unreachable": "✖", "joining": "●", "online": "●"}
